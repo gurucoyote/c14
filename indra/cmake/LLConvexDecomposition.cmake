642a2dce906e47ae8718c9788d83987f19271e58
Guru Coyote <gc@testbild.tv>
2011-11-13 15:59:12 +0100

    * use HACD on osx

--- indra/cmake/LLConvexDecomposition.cmake
+++ indra/cmake/LLConvexDecomposition.cmake
@@ -6,10 +6,7 @@ set(LLCONVEXDECOMP_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include)
 # Note: we don't have access to proprietary libraries, so it makes no sense
 #		bothering with the (INSTALL_PROPRIETARY AND NOT STANDALONE) case !
 
-if (DARWIN)
-	# I don't yet have a working HACD pre-built library for MacOS-X...
-	use_prebuilt_binary(llconvexdecompositionstub)
-	set(LLCONVEXDECOMP_LIBRARY llconvexdecompositionstub)
-else (DARWIN)
-	include(HACD)
-endif (DARWIN)
+#use_prebuilt_binary(llconvexdecompositionstub)
+#set(LLCONVEXDECOMP_LIBRARY llconvexdecompositionstub)
+
+include(HACD)
